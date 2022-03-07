24ebe225d7a69fe92d475f14491e61974ade2f78
NILES <aniospgh@gmail.com>
2022-03-06 20:34:10 -0500

    updated local server port

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ app.use(cors({
 
 app.use(cookieParser());
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
-    console.log('Server started on port ${PORT}');
+    console.log('Server started on port' + PORT);
 });
 
 app.use("/auth", require("./routers/userrouter"));
